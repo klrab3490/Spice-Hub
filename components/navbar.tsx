e6cde351bc020cb98b4709ad84b45d5c836b6e8b
adithyakb10 <adithyakb93@gmail.com>
2024-10-02 11:35:27 +0530

    feat: Add a nav button for login and signup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,33 @@
-"use client"
+"use client";
 
 import * as React from "react";
 import { ModeToggle } from "@/components/mode-toggle";
+import NavLoginButton from "./navloginbutton";
 
 export function Navbar() {
-    return (
-        <nav className="flex items-center justify-between py-4 px-8">
-            <div className="flex items-center space-x-4">
-                <a href="/" className="text-lg font-semibold">Home</a>
-                <a href="/about" className="text-lg font-semibold">About</a>
-            </div>
-            <ModeToggle />
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between py-4 px-8">
+      <div className="flex items-center space-x-4">
+        <a
+          href="/"
+          className="text-lg font-semibold"
+        >
+          Home
+        </a>
+        <a
+          href="/about"
+          className="text-lg font-semibold"
+        >
+          About
+        </a>
+      </div>
+      <div className="w-fit hidden md:block">
+        <NavLoginButton />
+      </div>
+
+      <div className="flex gap-5 items-center">
+        <ModeToggle />
+      </div>
+    </nav>
+  );
 }
---
+++ components/navloginbutton.tsx
@@ -0,0 +1,117 @@
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import {
+  CircleUserRound,
+  LogIn,
+  LogOut,
+  ShoppingCart,
+  UserPen,
+} from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function NavLoginButton() {
+  const { data: session } = useSession();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link
+              href={session ? "/profile" : "/login"}
+              legacyBehavior
+              passHref
+            >
+              {/* <NavigationMenuLink className={navigationMenuTriggerStyle()}> */}
+              <NavigationMenuTrigger>
+                <CircleUserRound />
+                <p className="text-[16px] pl-1">
+                  {session ? "Account" : "Login"}
+                </p>
+              </NavigationMenuTrigger>
+              {/* </NavigationMenuLink> */}
+            </Link>
+            <NavigationMenuContent>
+              <ul className="w-[180px] px-5 py-3">
+                <li className="w-full ">
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="text-sm text-center "
+                      href="/signup"
+                    >
+                      {session ? null : (
+                        <div className="flex gap-2 items-center">
+                          <LogIn />
+                          <p>Sign Up</p>
+                        </div>
+                      )}
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                {session ? null : (
+                  <div className={"w-full border border-gray-600 my-2"} />
+                )}
+                <li className="w-full">
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="text-sm text-center flex gap-2 items-center"
+                      href="/signup"
+                    >
+                      <UserPen />
+                      My Profile
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <div className="w-full border border-gray-600 my-2" />
+                <li className="w-full">
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="text-sm text-center flex gap-2 items-center"
+                      href="/signup"
+                    >
+                      <ShoppingCart />
+                      My Orders
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                {session ? (
+                  <div>
+                    <div className="w-full border border-gray-600 my-2" />
+                    <li className="w-full">
+                      <NavigationMenuLink asChild>
+                        <button
+                          className="text-sm text-center flex gap-2 items-center"
+                          onClick={() => signOut()}
+                        >
+                          <LogOut />
+                          Log Out
+                        </button>
+                      </NavigationMenuLink>
+                    </li>
+                  </div>
+                ) : null}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
